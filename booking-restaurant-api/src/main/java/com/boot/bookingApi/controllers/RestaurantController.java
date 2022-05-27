6dcbd706483404496ae80437d184a17cf04f36f6
Kevin Rios <KevinJIRios@gmail.com>
2022-05-27 10:24:52 -0300

    add jsonResponse y metodo en controller findById

--- booking-restaurant-api/src/main/java/com/boot/bookingApi/controllers/RestaurantController.java
+++ booking-restaurant-api/src/main/java/com/boot/bookingApi/controllers/RestaurantController.java
@@ -1,5 +1,26 @@
 package com.boot.bookingApi.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.boot.bookingApi.exceptions.BookingException;
+import com.boot.bookingApi.jsons.RestaurantRest;
+import com.boot.bookingApi.responses.BookingResponse;
+import com.boot.bookingApi.services.RestaurantService;
+
+@RestController
+@RequestMapping(path = "/booking-restaurant" + "v1")
 public class RestaurantController {
+	
+	@Autowired
+	RestaurantService restaurantService;
+	
+	
+	public BookingResponse<RestaurantRest> getRestaurantById(Long restaurantId) throws BookingException{
+		
+		return new BookingResponse<>("sucess", String.valueOf(HttpStatus.OK), "OK", restaurantService.getRestaurantById(restaurantId));
+	}
 
 }
