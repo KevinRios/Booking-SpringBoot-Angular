6c2698aabf03b7bb0d33a02a1402a6f33082d893
Kevin Rios <KevinJIRios@gmail.com>
2022-06-05 23:17:15 -0300

    add test unitarios para getRestaurants en restaurant

--- booking-restaurant-api/src/test/java/com/boot/bookingrestaurantapi/controller/RestaurantControllerTests.java
+++ booking-restaurant-api/src/test/java/com/boot/bookingrestaurantapi/controller/RestaurantControllerTests.java
@@ -20,43 +20,44 @@ import com.boot.bookingrestaurantapi.responses.BookingResponse;
 import com.boot.bookingrestaurantapi.services.RestaurantService;
 
 public class RestaurantControllerTests {
-	
+
 	private static final Long RESTAURANT_ID = 1L;
 	private static final String SUCCESS_STATUS = "Success";
 	private static final String SUCCESS_CODE = "200 OK";
 	private static final String OK = "OK";
+	
 	private static final RestaurantRest RESTAURANT_REST = new RestaurantRest();
 	private static final String NAME = "Hamburguesas";
 	private static final String DESCRIPTION = "Mucha comida";
 	private static final String ADRESS = "Calle calle";
 	private static final String IMAGE = "WWW.IMAGEN.COM";
 
-	private static final List<TurnRest> TURN_LIST = new ArrayList<>(); 
+	private static final List<TurnRest> TURN_LIST = new ArrayList<>();
+	private static final List<RestaurantRest> RESTAURANT_REST_LIST = new ArrayList<>();
 
-
-	@Mock	
+	@Mock
 	RestaurantService restaurantService;
-	
+
 	@InjectMocks
 	RestaurantController restaurantController;
-	
+
 	@Before
-	public void init() throws BookingException{
+	public void init() throws BookingException {
 		MockitoAnnotations.initMocks(this);
-		
+
 		RESTAURANT_REST.setId(RESTAURANT_ID);
 		RESTAURANT_REST.setName(NAME);
 		RESTAURANT_REST.setDescription(DESCRIPTION);
 		RESTAURANT_REST.setAdress(ADRESS);
 		RESTAURANT_REST.setImage(IMAGE);
 		RESTAURANT_REST.setTurns(TURN_LIST);
-		
+
 		// creo el servicio
 		Mockito.when(restaurantService.getRestaurantById(RESTAURANT_ID)).thenReturn(RESTAURANT_REST);
 	}
 
 	@Test
-	public void getRestaurantByIdTest() throws BookingException{
+	public void getRestaurantByIdTest() throws BookingException {
 		final BookingResponse<RestaurantRest> response = restaurantController.getRestaurantById(RESTAURANT_ID);
 		assertEquals(response.getStatus(), SUCCESS_STATUS);
 		assertEquals(response.getCode(), SUCCESS_CODE);
@@ -64,6 +65,15 @@ public class RestaurantControllerTests {
 		assertEquals(response.getData(), RESTAURANT_REST);
 
 	}
-	
-	
+
+	@Test
+	public void getRestaurantsTest() throws BookingException {
+		final BookingResponse<List<RestaurantRest>> response = restaurantController.getRestaurants();
+
+		assertEquals(response.getStatus(), SUCCESS_STATUS);
+		assertEquals(response.getCode(), SUCCESS_CODE);
+		assertEquals(response.getMessage(), OK);
+		assertEquals(response.getData(), RESTAURANT_REST_LIST);
+	}
+
 }
